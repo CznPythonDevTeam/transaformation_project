52a7bbd1cde7717e9caea35d68ade0e23a27f61b
Gautam Bharadwaj <gautam.bharadwaj@cognitivzen.com>
2024-03-31 15:13:02 +0530

    Modified main.py and Verified end to end testing

--- main.py
+++ main.py
@@ -4,32 +4,92 @@ from snowflake.connector.pandas_tools import write_pandas
 from dotenv import load_dotenv
 import os
 import pandas as pd
+from confluent_kafka import Consumer, KafkaError
 
-def get_sf_connection():
-  '''
-  Read the Snowflake configuration and save them in the environment variables. For Example,
-  the Snowfalke account will be in environment variable SF_ACCOUNT.
-  '''
+class snowfalke_db_manager():
 
-  # Read the Snowflake configuration
-  load_dotenv("./.virsec_sf_config.env")
+  def __init__(self):  
+    # Create cursor
+    self.sf_conn, self.sf_cur = self.get_sf_connection()
 
-  # Creating Snowflake connection with the user account deatils
-  sf_conn = snow.connect(
-    account=os.environ.get('SF_ACCOUNT'),
-    user=os.environ.get('SF_USER'),
-    password=os.environ.get('SF_PWD'),
-    warehouse=os.environ.get('SF_WAREHOUSE'),
-    role=os.environ.get('SF_ROLE'),
-    database=os.environ.get('SF_DATABASE'),
-    schema=os.environ.get('SF_SCHEMA')
-  )
+  def get_sf_connection(self):
+    '''
+    Read the Snowflake configuration and save them in the environment variables. For Example,
+    the Snowfalke account will be in environment variable SF_ACCOUNT.
+    '''
 
-  # Create cursor
-  sf_cur = sf_conn.cursor()
-  return sf_conn, sf_cur
+    # Read the Snowflake configuration
+    load_dotenv("./.virsec_sf_config.env")
+
+    # Creating Snowflake connection with the user account deatils
+    sf_conn = snow.connect(
+      account=os.environ.get('SF_ACCOUNT'),
+      user=os.environ.get('SF_USER'),
+      password=os.environ.get('SF_PWD'),
+      warehouse=os.environ.get('SF_WAREHOUSE'),
+      role=os.environ.get('SF_ROLE'),
+      database=os.environ.get('SF_DATABASE'),
+      schema=os.environ.get('SF_SCHEMA')
+    )
+
+    sf_cur = sf_conn.cursor()
+    return sf_conn, sf_cur
+
+  def push_snowflake_db(self,dataframe):  
+    print(dataframe,"dataframe")
+    success, nchunks, nrows, _ = write_pandas(self.sf_conn,
+                                                      df=dataframe,
+                                                      table_name="murali_test_systemintegrity",
+                                                      auto_create_table=True,
+                                              schema="DEV_SCHEMA")
 
 
+class kafka_consumer_data():
+  
+  def __init__(self):
+    self.conf = {
+        'bootstrap.servers': 'localhost:9092',  # Kafka broker address
+        'group.id': 'abc',        # Consumer group ID
+        'auto.offset.reset': 'earliest'         # Start consuming from the earliest message
+    }
+
+    self.topic_names = ['test_topic2']
+
+  def get_json_data(self):
+    # Create Kafka consumer
+    consumer = Consumer(self.conf)
+    # Subscribe to Kafka topic
+    consumer.subscribe(self.topic_names)
+    
+    # Poll for new messages
+    accumulated_data = []
+    try:
+        while True:
+            msg = consumer.poll(2.0)  # Poll for new messages, with a timeout of 2 second
+            print(msg)
+            if msg is None:
+                continue
+            if msg.error():
+                if msg.error().code() == KafkaError._PARTITION_EOF:
+                    # End of partition, the consumer reached the end of the topic
+                    continue
+                else:
+                    # Other error
+                    print(msg.error())
+                    break
+            # Print the consumed message
+            msg_value = msg.value().decode('utf-8')
+            print('Received message: {}'.format(msg_value))
+            accumulated_data.append(msg_value)
+            if len(accumulated_data)==3:
+                break
+            # return msg_value
+    except KeyboardInterrupt:
+        # Stop consuming when Ctrl+C is pressed
+        consumer.close()
+    return accumulated_data
+
+'''
 app_instance_data={
   "_id": {
     "_id": {
@@ -204,6 +264,7 @@ process_details_data={
   "operationType": "insert",
   "tenantId": "BOA"
 }
+'''
 
 # df=pd.DataFrame({
 #     "TenantID":["BOA"],
@@ -219,44 +280,53 @@ process_details_data={
 #     "timestamp":[""]
 #     })
 
-import json
+def json_data_transformation(app_instance_data,ds_installer_data,process_details_data):
 
-json_file_path = 'SystemIntegrity/system_integrity_schema.json'
-with open(json_file_path, 'r') as json_file:
-  data_dict = json.load(json_file)
+  df=pd.DataFrame()
 
-APPLICATION_INSTANCE = pd.json_normalize(app_instance_data)
-ds_installer = pd.json_normalize(ds_installer_data)
-process_detail = pd.json_normalize(process_details_data)
+  ## added code ##
+  import json
+  json_file_path = 'SystemIntegrity/system_integrity_schema.json'
+  with open(json_file_path, 'r') as json_file:
+      data_dict = json.load(json_file)
 
-hardcoded_dict = {'APPLICATION_INSTANCE': APPLICATION_INSTANCE, 'ds_installer': ds_installer,
-                  'process_detail': process_detail}
+  APPLICATION_INSTANCE = pd.json_normalize(app_instance_data)
+  ds_installer = pd.json_normalize(ds_installer_data)
+  process_detail = pd.json_normalize(process_details_data)
 
-row_data = {}
-for col_key, col_data in data_dict.items():
-  if col_data:
-    source_df = col_data['source']
-    source_df_name = hardcoded_dict[source_df]
-    source_key = col_data['key']
-    column_name = next(filter(lambda col: source_key in col, source_df_name.columns), None)
-    if column_name:
-      val = source_df_name[column_name].values
-      row_data[col_key] = val
-    else:
-      row_data[col_key] = ''
-  else:
-    row_data[col_key] = ''
+  hardcoded_dict = {'APPLICATION_INSTANCE':APPLICATION_INSTANCE,'ds_installer':ds_installer,'process_detail':process_detail}
 
-output_df = pd.DataFrame(row_data)
+  row_data = {}
+  for col_key, col_data in data_dict.items():
+      if col_data:
+          source_df = col_data['source']
+          source_df_name = hardcoded_dict[source_df]
+          source_key = col_data['key']
+          column_name = next(filter(lambda col: source_key in col, source_df_name.columns), None)
+          if column_name:
+              val = source_df_name[column_name].values
+              row_data[col_key] = val
+          else:
+              row_data[col_key] = ''
+      else:
+          row_data[col_key] = ''
 
-df=pd.DataFrame()
+  output_df = pd.DataFrame(row_data)
+  return output_df
 
-conn, cursor = get_sf_connection()
-success, nchunks, nrows, _ = write_pandas(conn,
-                                          df=output_df,
-                                          table_name="murali_test_systemintegrity",
-                                          auto_create_table=True,
-                                          schema="DEV_SCHEMA")
+
+kafka_consumer = kafka_consumer_data()
+consumer_json_data = kafka_consumer.get_json_data()
+
+app_instance_data = eval(consumer_json_data[0])
+ds_installer_data = eval(consumer_json_data[1])
+process_details_data = eval(consumer_json_data[2])
+
+output_df = json_data_transformation(app_instance_data,ds_installer_data,process_details_data)
+print(output_df,"output_df")
+
+sf_db = snowfalke_db_manager()
+sf_db.push_snowflake_db(output_df)
 
 # p = Producer({'bootstrap.servers': 'localhost:9092'})
 # p.produce('mytopic', 'my message')
